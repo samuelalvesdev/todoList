28dedcd989fbb9a92b1cbc4a1b225ed19a70c342
Samuel Alves <samuelalves@utic.pe.senac.br>
2023-10-11 00:09:44 -0300

    transpilação feita para js

--- public/dist/index.js
+++ public/dist/index.js
@@ -1,2 +1,25 @@
 "use strict";
-alert("Bora iniciar esse projeto de TS!! :)");
+(function () {
+    var todo = {
+        description: "todo",
+        done: false,
+    };
+    var reminder = {
+        description: "reminder",
+        date: "15-12-2021",
+    };
+    var taskView = {
+        render: function (tasks) {
+            var taskList = document.getElementById("tasksList");
+            while (taskList === null || taskList === void 0 ? void 0 : taskList.firstChild) {
+                taskList.removeChild(taskList.firstChild);
+            }
+            tasks.forEach(function (task) {
+                var li = document.createElement("LI");
+                var textNode = document.createTextNode(JSON.stringify(task));
+                li.appendChild(textNode);
+                taskList === null || taskList === void 0 ? void 0 : taskList.appendChild(li);
+            });
+        },
+    };
+})();
