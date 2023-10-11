9a02f0ef48082d695aa4a9f94f99cf5d4181017a
Samuel Alves <samuelalves@utic.pe.senac.br>
2023-10-11 00:07:04 -0300

    primeiras constantes do todoList

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
-alert("Bora iniciar esse projeto de TS!! :)");
+(() => {
+  const todo = {
+    description: "todo",
+    done: false,
+  };
+
+  const reminder = {
+    description: "reminder",
+    date: "15-12-2021",
+  };
+
+  const taskView = {
+    render(tasks: Array<Object>) {
+      const taskList = document.getElementById("tasksList");
+      while (taskList?.firstChild) {
+        taskList.removeChild(taskList.firstChild);
+      }
+
+      tasks.forEach((task) => {
+        const li = document.createElement("LI");
+        const textNode = document.createTextNode(JSON.stringify(task));
+        li.appendChild(textNode);
+        taskList?.appendChild(li);
+      });
+    },
+  };
+})();
